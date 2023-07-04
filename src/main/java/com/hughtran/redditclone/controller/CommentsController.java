2a30d237134eefa879dfa74f14b4da6864166d25
unknown <thien.hieu2299@gmail.com>
2023-07-04 14:54:04 +0700

    add comment controller

---
+++ src/main/java/com/hughtran/redditclone/controller/CommentsController.java
@@ -0,0 +1,37 @@
+package com.hughtran.redditclone.controller;
+
+import com.hughtran.redditclone.dto.CommentsDto;
+import com.hughtran.redditclone.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+@AllArgsConstructor
+public class CommentsController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(params = "postId")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@RequestParam Long postId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(commentService.getAllCommentsForPost(postId));
+    }
+
+    @GetMapping(params = "userName")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@RequestParam String userName){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(commentService.getAllCommentsForUser(userName));
+    }
+
+}
---
+++ src/main/java/com/hughtran/redditclone/service/CommentService.java
@@ -0,0 +1,68 @@
+package com.hughtran.redditclone.service;
+
+import com.hughtran.redditclone.dto.CommentsDto;
+import com.hughtran.redditclone.exceptions.PostNotFoundException;
+import com.hughtran.redditclone.exceptions.SpringRedditException;
+import com.hughtran.redditclone.mapper.CommentMapper;
+import com.hughtran.redditclone.model.Comment;
+import com.hughtran.redditclone.model.NotificationEmail;
+import com.hughtran.redditclone.model.Post;
+import com.hughtran.redditclone.model.User;
+import com.hughtran.redditclone.repository.CommentRepository;
+import com.hughtran.redditclone.repository.PostRepository;
+import com.hughtran.redditclone.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+    private static final String POST_URL = "";
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    public void save(CommentsDto commentsDto) {
+        Post post = postRepository.findById(commentsDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
+        Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(post.getUser().getUsername() + " posted a comment on your post." + POST_URL);
+        sendCommentNotification(message, post.getUser());
+    }
+
+    private void sendCommentNotification(String message, User user) {
+        mailService.sendMail(new NotificationEmail(user.getUsername() + " Commented on your post", user.getEmail(), message));
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(postId.toString()));
+        return commentRepository.findByPost(post)
+                .stream()
+                .map(commentMapper::mapToDto).toList();
+    }
+
+    public List<CommentsDto> getAllCommentsForUser(String userName) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+        return commentRepository.findAllByUser(user)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .toList();
+    }
+
+    public boolean containsSwearWords(String comment) {
+        if (comment.contains("shit")) {
+            throw new SpringRedditException("Comments contains unacceptable language");
+        }
+        return false;
+    }
+}
